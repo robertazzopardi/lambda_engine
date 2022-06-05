09ed1f41cdbc56e8185078c66871709f3c66d321
robertazzopardi <robertazzopardi@icloud.com>
2022-06-05 22:20:29 +0100

    removed comments from cube example

--- examples/cube/main.rs
+++ examples/cube/main.rs
@@ -5,13 +5,6 @@ fn main() {
 
     let mut camera = Camera::new(-2., 1., 0.);
 
-    // let cube = GeometryBuilder::default()
-    //     .properties(CubeInfoBuilder::default().radius(0.5).build().unwrap())
-    //     .cull_mode(ModelCullMode::Back)
-    //     .shader(ShaderType::Vertex)
-    //     .indexed()
-    //     .build()
-    //     .unwrap();
     let cube = Cube::new(
         GeometryBuilder::default()
             .properties(CubeInfoBuilder::default().radius(0.5).build().unwrap())
@@ -28,19 +21,3 @@ fn main() {
 
     engine.run(&mut display, camera);
 }
-
-// fn test() {
-//     let cube = Cube::new(
-//         GeometryBuilder::default()
-//             .properties(CubeInfoBuilder::default().radius(0.5).build().unwrap())
-//             .cull_mode(ModelCullMode::Back)
-//             .shader(ShaderType::Vertex)
-//             .indexed()
-//             .build()
-//             .unwrap(),
-//     );
-
-//     let m: Geom = cube.into();
-
-//     dbg!(m.vertices_and_indices());
-// }
